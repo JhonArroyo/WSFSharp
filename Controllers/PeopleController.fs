d147a9de2d42ac6ac26be61a1694d36b13454039
JhonArroyo <jhonatanarroyo97@outlook.com>
2022-03-06 22:48:31 -0500

    First Commit

---
+++ Controllers/PeopleController.fs
@@ -0,0 +1,28 @@
+﻿namespace WSFSharp.Controllers
+
+open Microsoft.AspNetCore.Mvc
+open System.Collections.Generic
+
+//module PeopleController
+type Person = 
+    {name : string; 
+     age : int; 
+     url : string;}
+
+[<ApiController>]
+[<Route("[controller]")>]
+type PeopleController() = 
+    inherit ControllerBase()
+
+    let people = {
+        name= "Jhon";
+        age= 24;
+        url= "https://localhost/";}
+
+    [<HttpGet>]
+    member _.Get() =
+        //"WEB API SERVICE WITH F#"
+        base.Ok(people)
+        
+
+
